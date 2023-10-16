5dc1605c2563d9517ac4d7e45d3bc9ac32bfef02
Hanoch Mordkovich <hanoch.mordkovich@techsee.me>
2023-10-16 16:33:32 +0300

    first commit

---
+++ SwiftyLibraryExamples/SwiftyLibraryExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibraryExamplesApp.swift
+//  SwiftyLibraryExamples
+//
+//  Created by Hanoch Mordkovich on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibraryExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
