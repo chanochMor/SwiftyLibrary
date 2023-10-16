78ce71ca6238d012020e1c4bf4fba7271b7beaa4
Hanoch Mordkovich <hanoch.mordkovich@techsee.me>
2023-10-16 15:52:29 +0300

    Initial Commit

---
+++ SwiftyLibrary/SwiftyLibrary.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibrary.h
+//  SwiftyLibrary
+//
+//  Created by Hanoch Mordkovich on 16/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibrary.
+FOUNDATION_EXPORT double SwiftyLibraryVersionNumber;
+
+//! Project version string for SwiftyLibrary.
+FOUNDATION_EXPORT const unsigned char SwiftyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibrary/PublicHeader.h>
+
+
