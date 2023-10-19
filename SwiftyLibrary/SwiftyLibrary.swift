3586a4fa3daef108fa2b0b7b0b5ea02705e56894
Hanoch Mordkovich <hanoch.mordkovich@techsee.me>
2023-10-19 15:14:56 +0300

    add test case

---
+++ SwiftyLibrary/SwiftyLibrary.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLibrary.swift
+//  SwiftyLibrary
+//
+//  Created by Hanoch Mordkovich on 19/10/2023.
+//
+
+import Foundation
+
+public final class SwiftyLibrary {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLibraryTests/SwiftyLibraryTests.swift
+++ SwiftyLibraryTests/SwiftyLibraryTests.swift
@@ -10,6 +10,16 @@ import XCTest
 
 final class SwiftyLibraryTests: XCTestCase {
 
+    var swiftyLib: SwiftyLibrary!
+
+        override func setUp() {
+            swiftyLib = SwiftyLibrary()
+        }
+
+        func testAdd() {
+            XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+        }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
