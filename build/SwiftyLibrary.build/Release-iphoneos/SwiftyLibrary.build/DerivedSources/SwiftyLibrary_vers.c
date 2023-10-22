117a530c9617094593f758afe408345161ec30ff
Hanoch Mordkovich <hanoch.mordkovich@techsee.me>
2023-10-22 14:11:26 +0300

    add podspec

---
+++ build/SwiftyLibrary.build/Release-iphoneos/SwiftyLibrary.build/DerivedSources/SwiftyLibrary_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char SwiftyLibraryVersionString[];
+ extern const double SwiftyLibraryVersionNumber;
+
+ const unsigned char SwiftyLibraryVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftyLibrary  PROJECT:SwiftyLibrary-1" "\n";
+ const double SwiftyLibraryVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/SwiftyLibrary.build/Release-iphoneos/SwiftyLibrary.build/Objects-normal/arm64/SwiftyLibrary_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/hanoch.mordkovich/Desktop/TechSee-Frameworks/SwiftyLibrary/build/SwiftyLibrary.build/Release-iphoneos/SwiftyLibrary.build/module.modulemap \
+  /Users/hanoch.mordkovich/Desktop/TechSee-Frameworks/SwiftyLibrary/build/SwiftyLibrary.build/Release-iphoneos/SwiftyLibrary.build/DerivedSources/SwiftyLibrary_vers.c
